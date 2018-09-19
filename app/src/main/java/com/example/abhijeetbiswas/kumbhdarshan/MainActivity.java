9f2b2b37c69c4d6c80a433e224bcabc163513761
abhi-biswas <mpsabhijeet@gmail.com>
2018-09-19 14:50:59 +0530

    Added navigation Menu and dummy Fragments.
    Signed-off-by: abhi-biswas <mpsabhijeet@gmail.com>

--- app/src/main/java/com/example/abhijeetbiswas/kumbhdarshan/MainActivity.java
+++ app/src/main/java/com/example/abhijeetbiswas/kumbhdarshan/MainActivity.java
@@ -1,14 +1,49 @@
 package com.example.abhijeetbiswas.kumbhdarshan;
 
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.*;
 
-public class MainActivity extends AppCompatActivity {
-
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+    private DrawerLayout drawerLayout;
+    private ActionBarDrawerToggle mtoggle;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        drawerLayout=(DrawerLayout) findViewById(R.id.drawer);
+        mtoggle= new ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close);
+        drawerLayout.addDrawerListener(mtoggle);
+        mtoggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        NavigationView navigationView=(NavigationView) findViewById(R.id.nav);
+        navigationView.setNavigationItemSelectedListener(this);
+        Fragment frag = new home();
+        FragmentManager fm = getSupportFragmentManager();
+        FragmentTransaction ft = fm.beginTransaction();
+        ft.replace(R.id.frag3,frag);
+        ft.commit();
+    }
+
+    @Override
+   public boolean onOptionsItemSelected(MenuItem item) {
+        if(mtoggle.onOptionsItemSelected(item))
+            return true;
+
+
+        return super.onOptionsItemSelected(item);
     }
 
     @Override
@@ -24,6 +59,49 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onPause() {
         super.onPause();
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+        Fragment frag = null;
+        //Fragment curr= getSupportFragmentManager().findFragmentById(R.id.frag3);
+        if(id==R.id.nav_places)
+        {
+            frag = (Fragment)new Places();
+
+        }
+        if(id==R.id.nav_faq)
+        {
+            frag = (Fragment)new faq();
+
+        }
+        if(id==R.id.nav_contacts)
+        {
+            frag=new contacts();
+        }
+        if(id==R.id.nav_home)
+        {
+            frag = (Fragment)new home();
+
+        }
+        if(id==R.id.nav_about)
+        {
+            frag = (Fragment)new About();
+
+        }
+
+        if (frag!=null)
+        {
+            FragmentManager fm = getSupportFragmentManager();
+            FragmentTransaction ft = fm.beginTransaction();
+            ft.replace(R.id.frag3,frag);
+            ft.commit();
+        }
+        drawerLayout=(DrawerLayout) findViewById(R.id.drawer);
+        drawerLayout.closeDrawer(GravityCompat.START);
+
+        return true;
     }
 }
 
