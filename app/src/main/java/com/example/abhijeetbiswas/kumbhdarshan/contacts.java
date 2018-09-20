12c03fcf3f95fadff4c10e85a82be43c9b48dddc
Shrikant <shrikantpattawi@gmail.com>
2018-09-21 00:05:19 +0530

    Contacts added

--- app/src/main/java/com/example/abhijeetbiswas/kumbhdarshan/contacts.java
+++ app/src/main/java/com/example/abhijeetbiswas/kumbhdarshan/contacts.java
@@ -1,13 +1,20 @@
 package com.example.abhijeetbiswas.kumbhdarshan;
 
+import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageInfo;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
+import android.widget.Button;
+import android.widget.TextView;
+import android.content.pm.PackageManager;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -49,60 +56,111 @@ public class contacts extends Fragment {
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }*/
-
+    View view;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_contacts, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    /*public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
-        }
+       view= inflater.inflate(R.layout.fragment_contacts, container, false);
+        Button button=(Button)view.findViewById(R.id.button4);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String phoneNumber = String.format("tel:+915322500775");
+                Intent dialIntent = new Intent(Intent.ACTION_DIAL);
+                dialIntent.setData(Uri.parse(phoneNumber));
+                startActivity(dialIntent);
+            }
+        });
+        Button button2=(Button)view.findViewById(R.id.button6);
+        button2.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // click bottom code here
+                Intent sendIntent = new Intent(Intent.ACTION_SEND);
+                String[] add={"Test@gmail.com"};
+                sendIntent.putExtra(Intent.EXTRA_EMAIL,add);
+                sendIntent.putExtra(Intent.EXTRA_SUBJECT,"Subject");
+                sendIntent.putExtra(Intent.EXTRA_TEXT,"Body");
+                sendIntent.setType("message/rfc822");
+                startActivity(Intent.createChooser(sendIntent,"Send email..."));
+            }
+        });
+        Button button3=(Button)view.findViewById(R.id.button7);
+        button3.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String phoneNumber = String.format("tel:+915322504011");
+                Intent dialIntent = new Intent(Intent.ACTION_DIAL);
+                dialIntent.setData(Uri.parse(phoneNumber));
+                startActivity(dialIntent);
+            }
+        });
+        Button button4=(Button)view.findViewById(R.id.button8);
+        button4.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String phoneNumber = String.format("tel:101");
+                Intent dialIntent = new Intent(Intent.ACTION_DIAL);
+                dialIntent.setData(Uri.parse(phoneNumber));
+                startActivity(dialIntent);
+            }
+        });
+        Button button5=(Button)view.findViewById(R.id.button9);
+        button5.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String phoneNumber = String.format("tel:102");
+                Intent dialIntent = new Intent(Intent.ACTION_DIAL);
+                dialIntent.setData(Uri.parse(phoneNumber));
+                startActivity(dialIntent);
+            }
+        });
+        Button button6=(Button)view.findViewById(R.id.button10);
+        button6.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String phoneNumber = String.format("tel:100");
+                Intent dialIntent = new Intent(Intent.ACTION_DIAL);
+                dialIntent.setData(Uri.parse(phoneNumber));
+                startActivity(dialIntent);
+            }
+        });
+        Button button7=(Button)view.findViewById(R.id.button11);
+        button7.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String phoneNumber = String.format("tel:108");
+                Intent dialIntent = new Intent(Intent.ACTION_DIAL);
+                dialIntent.setData(Uri.parse(phoneNumber));
+                startActivity(dialIntent);
+            }
+        });
+        return view;
     }
 
   /*  @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
-    }
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    /*public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }*/
+   }*/
+
+
+  /*
+  Intent intent = new Intent(android.content.Intent.ACTION_SEND);
+intent.setType("text/html");
+// intent.setType("text/plain");
+final PackageManager pm = getPackageManager();
+final List<ResolveInfo> matches = pm.queryIntentActivities(intent, 0);
+ResolveInfo best = null;
+for (final ResolveInfo info : matches) {
+    if (info.activityInfo.packageName.endsWith(".gm") || info.activityInfo.name.toLowerCase().contains("gmail")) {
+        best = info;
+        break;
+    }
+}
+if (best != null) {
+    intent.setClassName(best.activityInfo.packageName, best.activityInfo.name);
+}
+intent.putExtra(android.content.Intent.EXTRA_SUBJECT, "YOUR SUBJECT");
+intent.putExtra(android.content.Intent.EXTRA_TEXT, Html.fromHtml("YOUR EXTRAS"));
+
+startActivity(intent);
+*/
+
 }
