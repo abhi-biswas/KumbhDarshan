7e8adf2337dcb656504ffeca04f68f36e35c82ec
abhi-biswas <mpsabhijeet@gmail.com>
2018-09-21 17:16:39 +0530

    Added framework for home

--- app/src/main/java/com/example/abhijeetbiswas/kumbhdarshan/home_sc.java
+++ app/src/main/java/com/example/abhijeetbiswas/kumbhdarshan/home_sc.java
@@ -1,22 +1,19 @@
 package com.example.abhijeetbiswas.kumbhdarshan;
 
-import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-
-
-public class home extends Fragment {
+public class home_sc extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        View view =inflater.inflate(R.layout.fragment_home_sc, container, false);
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        return view;
     }
 
 
